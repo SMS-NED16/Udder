3bbae36a48e2637cb6aadb1fb7456a82847f4b90
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-02-27 16:25:01 +0500

    Add a new print statement to the viewDidLoad function for ViewController.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print( "Some random code. View will appear." )
+        print( "Another print statement for the console." )
         // a random comment
     }
     
