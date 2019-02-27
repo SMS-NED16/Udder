0d7abb9cc1e85284e245b032c44925cbc036a557
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-02-27 16:15:54 +0500

    Add a comment and a print statement to viewDidLoad function in ViewController file.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print( "Some random code. View will appear." )
+        // a random comment
     }
-
-
+    
+    
 }
 
